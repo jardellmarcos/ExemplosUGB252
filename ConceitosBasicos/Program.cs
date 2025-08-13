45db6ea2e3f223ede8501439bef8a730e911deee
jardellmarcos <jardelcmarcos@gmail.com>
2025-08-12 21:05:15 -0300

    metade do codigo

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,5 +1,16 @@
-﻿Console.WriteLine("Informe seu nome ");
+﻿using System.ComponentModel.Design;
+
+Console.WriteLine("Informe seu nome ");
 string nome = Console.ReadLine();
 Console.WriteLine("Informe seu salário: ");
 decimal salario = decimal.Parse(Console.ReadLine());
 Console.WriteLine($"Seu nome é {nome} e seu salario {salario}");
+
+//informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
+if (salario <= 2.259,20m);
+{
+    Console.WriteLine("Insento");
+}
+else if (salario >= 2.259,21m && salario <= 2826.65)
+}
+    decimal novoSalario = salario - (salario <= 2)
