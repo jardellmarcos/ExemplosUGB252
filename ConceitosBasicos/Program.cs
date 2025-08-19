14fc8a6f79df91a8d888b3066e160ab849245190
jardellmarcos <jardelcmarcos@gmail.com>
2025-08-19 19:13:53 -0300

    Atualizar o Program.cs

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,16 +1,57 @@
-﻿using System.ComponentModel.Design;
-
-Console.WriteLine("Informe seu nome ");
-string nome = Console.ReadLine();
-Console.WriteLine("Informe seu salário: ");
-decimal salario = decimal.Parse(Console.ReadLine());
-Console.WriteLine($"Seu nome é {nome} e seu salario {salario}");
-
-//informem o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
-if (salario <= 2.259,20m);
+Console. WriteLine("Informe seu nome: "):
+string nome = Console. ReadLine():
+Console WriteLine("Informe seu salário: "):
+decimal salario = decimal. Parse(Console.ReadLine());
+Console WriteLine("Seja bem-vindo(a) " + nome +". Seu salário é de R$" + salario);
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+//informe o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real if (salario <= 2259.20m)
 {
-    Console.WriteLine("Insento");
+Console WriteLine("Você não paga taxa.");
 }
-else if (salario >= 2.259,21m && salario <= 2826.65)
+else
+if (salario >= 2259.20m && salario <= 2826.65m)
+{
+decimal novoSalario = salario - (salario * 0.075m);
+Console WriteLine($"Seu salário com desconto é de {novoSalario:C2}");
 }
-    decimal novoSalario = salario - (salario <= 2)
+{
+else if (salario >= 2826.66m && salario <= 3751.05m)
+decimal novoSalario = salario - (salario * 0.15m);
+Console WriteLine($"Seu salário com desconto é de {novoSalario: C2}");
+}
+{
+else if (salario >= 3751.06m && salario <= 4664.68m)
+decimal novoSalario = salario - (salario * 0.225m):
+Console WriteLine($"Seu salário com desconto é de {novoSalario:C2}"):
+}
+else
+{
+decimal novoSalario = salario - (salario * 0.275m);
+Console WriteLine($"Seu salário com desconto é de {novoSalario:C2}");
